f047aebe30618d71f243e34fe123feb1651df241
Angel <tic-290096@utnay.edu.mx>
2024-11-07 19:31:57 -0700

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,7 +13,7 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies = [
-        '192.168.1.254'
+        '35.212.94.98'
     ];
 
     /**
