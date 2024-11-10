8f6d73b41e9602dcd4679531b33d8def348d730c
Angel <tic-290096@utnay.edu.mx>
2024-11-09 23:11:53 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then(res => console.log('Service Worker registrado correctamente', res))
-        .catch(err => console.log('Error al registrar el Service Worker', err));
+    window.addEventListener('load', () => {
+        // Verificar si ya hay un Service Worker registrado
+        if (!navigator.serviceWorker.controller) {
+            navigator.serviceWorker.register('./sw.js')
+                .then(res => console.log('Service Worker registrado correctamente', res))
+                .catch(err => console.log('Error al registrar el Service Worker', err));
+        } else {
+            console.log('El Service Worker ya está controlando esta página.');
+        }
+    });
 } else {
     console.log('Service Worker no soportado en este navegador');
 }
