f5000a3bae9c204b070be0b363330188d387fadd
Angel <tic-290096@utnay.edu.mx>
2024-11-11 14:46:19 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Asignar nombre y versión al caché
-const CACHE_VERSION = 'v2'; // Actualiza el número de versión cuando realices cambios significativos
+const CACHE_VERSION = 'v5'; // Actualiza el número de versión cuando realices cambios significativos
 const CACHE_NAME = `static-${CACHE_VERSION}`;
 
 // Archivos a guardar
@@ -24,6 +24,7 @@ var urlsToCache = [
   '/package.json',
   '/sw.js',
   '/main.js',
+  '/offline.html', // Agregar archivo offline si se desea
 ];
 
 // Evento de instalación del Service Worker
@@ -31,15 +32,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        // Almacenar todos los archivos en el caché
         return cache.addAll(urlsToCache)
-          .then(() => {
-            // Indicar al navegador que este nuevo Service Worker está listo
-            self.skipWaiting();
+          .then(() => self.skipWaiting()) // Service Worker listo
+          .catch(error => {
+            console.error('Error al almacenar en caché:', error);
           });
-      })
-      .catch(error => {
-        console.error('Error al almacenar en caché:', error);
       })
   );
 });
@@ -48,19 +45,14 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheName !== CACHE_NAME) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-      .then(() => {
-        // Indicar al navegador que este Service Worker se haga cargo de las solicitudes
-        return self.clients.claim();
-      })
+      .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Borrar cachés antiguos
+          }
+        })
+      ))
+      .then(() => self.clients.claim()) // Tomar control inmediato
   );
 });
 
@@ -68,26 +60,17 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Si se encuentra en caché, devolver la respuesta
-        }
-
-        return fetch(event.request)
-          .then(networkResponse => {
-            // Verifica que la respuesta sea válida antes de guardarla en caché
-            if (networkResponse && networkResponse.status === 200) {
-              return caches.open(CACHE_NAME).then(cache => {
-                cache.put(event.request, networkResponse.clone());
-                return networkResponse;
-              });
-            }
-            return networkResponse;
-          })
-          .catch(() => {
-            // Manejar errores de red, por ejemplo, mostrando una página de error personalizada
-            return caches.match('/offline.html');
-          });
-      })
+      .then(response => response || fetch(event.request)
+        .then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200) {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            });
+          }
+          return networkResponse;
+        })
+        .catch(() => caches.match('/offline.html')) // Opcional
+      )
   );
 });
